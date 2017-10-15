70806d7c71356eb6b0372fcfd37f83a4f16badc6
Deniz Karakay <androidkarakay@gmail.com>
2017-10-15 15:10:02 +0300

    Update Welcome_CodeKAL.java

--- Programming Languages/Java/Welcome_CodeKAL.java
+++ Programming Languages/Java/Welcome_CodeKAL.java
@@ -2,7 +2,7 @@ public class CodeKAL {
 
   public static void main (String[] args){
     //Prints "Welcome to CodeKAL"
-    System.out.println("Welcome to CodeKAL");
+    System.out.println("Welcome to CodeKAL!");
   
   }
 
